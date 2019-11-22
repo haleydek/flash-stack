fd31c0bd5fc0a450fb8070a9304e008af2ac4b25
haleydek <hcd09@live.com>
2019-11-21 22:47:14 -0600

    install styled-components. Add inline styling to body element. Create AppWrapper styled component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import styled from 'styled-components/macro';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <AppWrapper>
+      <h1>Hello World</h1>
+    </AppWrapper>
   );
 }
 
 export default App;
+
+const font = "Roboto, sans-serif";
+
+const AppWrapper = styled.div`
+  box-sizing: border-box;
+  font-family: ${font};
+  overflow: hidden;
+`;
