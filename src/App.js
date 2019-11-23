797a92dde292a31cda74107ed28c3741b5189cda
haleydek <hcd09@live.com>
2019-11-22 19:17:44 -0600

    create header components and add media screen sizes to AppWrapper

--- src/App.js
+++ src/App.js
@@ -13,8 +13,24 @@ export default App;
 
 const font = "Roboto, sans-serif";
 
+const device = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px'
+}
+
 const AppWrapper = styled.div`
   box-sizing: border-box;
   font-family: ${font};
   overflow: hidden;
+
+  @media only screen and (min-width: ${device.tablet}) {
+    
+  }
+
+  @media only screen and (min-width: ${})
 `;
