8cd0eaaaea51d44979fe8cbfc6cedbb5a9671ee6
haleydek <hcd09@live.com>
2019-12-08 16:21:20 -0600

    update indentation of styled components.

--- src/App.js
+++ src/App.js
@@ -55,31 +55,31 @@ const AppWrapper = styled.div`
 `;
 
 const PageWrapper = styled.div`
-position: relative;
-width: 100vw;
-height: 100%;
-margin: 0;
-padding: 0;
-overflow: auto;
+  position: relative;
+  width: 100vw;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: auto;
 `;
 
 const PageInner = styled.div`
 
-    @media only screen and (max-width: 767px) {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        text-align: center;
-        align-items: center;
-    }
+  @media only screen and (max-width: 767px) {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
+      align-items: center;
+  }
 
-    @media only screen and (min-width: 768px) {
-        max-width: 1440px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
+  @media only screen and (min-width: 768px) {
+      max-width: 1440px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
 
-        margin: 0 auto;
-    }
+      margin: 0 auto;
+  }
 `;
